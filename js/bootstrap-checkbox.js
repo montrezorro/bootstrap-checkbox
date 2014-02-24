3c0b667ca9b6488ae7bab57e24a8965f04b47dc6
Roberto <info@robertomontresor.it>
2014-02-24 14:03:08 +0100

    added version
    added version. start with 1.0.0

--- js/bootstrap-checkbox.js
+++ js/bootstrap-checkbox.js
@@ -1,7 +1,7 @@
 /* ===========================================================
- * bootstrap-checkbox.js
+ * bootstrap-checkbox - v.1.0.0
  * ===========================================================
- * Copyright 2013 Roberto Montresor
+ * Copyright 2014 Roberto Montresor
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
